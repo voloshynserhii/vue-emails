f0739514ee1617d67739b93b0e9191e5d9a5916f
Brendan Mulholland <github@bmulholland.ca>
2021-02-04 13:30:17 +0100

    Documentation clarifications

--- components/ToggleSortButton.vue
+++ components/ToggleSortButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <!--  the store mutation (markAllRead) is in store/emails.js -->
+  <!--  the store mutation (toggleSortEmails) is in store/index.js -->
   <button
     type="button"
     class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white border border-transparent rounded-md shadow-sm bg-navy-600 hover:bg-navy-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-navy-500"
